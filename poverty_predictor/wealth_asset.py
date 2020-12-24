bf8b67accd5166216d53d8ffbc401a781ba23944
Aishwarya Wahane <aishwarya.wahane@gmail.com>
2020-12-24 18:24:50 +0900

    Created functions for getting wealth and GPS data

---
+++ poverty_predictor/wealth_asset.py
@@ -0,0 +1,32 @@
+# Reading the DHS survey and GPS datasets
+
+# Importing necessary libraries
+from geopandas import read_file
+import pandas as pd
+
+# Creating a class to get wealth asset with location data
+class GetData():
+    def __init__(self, gps_path, survey_path):
+        self.gps_path = gps_path
+        self.survey_path = survey_path
+
+    # Creating a function to get GPS data
+    def gps_df(self):
+        shapefile_df = read_file(self.gps_path)
+        self.location_df = shapefile_df[['DHSCLUST', 'LATNUM', 'LONGNUM']]
+        return self.location_df
+
+    # Reading the survey (.dta/stata) file to get the wealth asset data
+    def survey_df(self):
+        dta_file = pd.read_stata(self.survey_path)
+        survey_df = dta_file[['hv001', 'hv270']].rename(columns={'hv001': 'DHSCLUST', 'hv270':'wealth_asset_index'})
+        survey_df['wealth_asset_index'] = survey_df['wealth_asset_index'].map({'poorest':1, 'poorer':2, 'middle':3, 'richer':4, 'richest':5})
+        survey_df['wealth_asset_index'] = survey_df['wealth_asset_index'].astype('int16')
+        self.survey_df = survey_df.groupby('DHSCLUST')[['wealth_asset_index']].median().reset_index()
+        return self.survey_df
+
+    # Merging location_df and survey_df to get wealth_df
+    def merged_wealth(self):
+        wealth_df = pd.merge(self.location_df, self.survey_df, on = 'DHSCLUST')
+        wealth_df = wealth_df.rename(columns={'DHSCLUST':'Cluster Number', 'LATNUM':'Latitude', 'LONGNUM':'Longitude','wealth_asset_index':'Asset Index'})
+        return wealth_df
